717387fb77c26b3fb05356e0fc80423dc72705dd
Yevgeniy Ivanov <anigeka@gmail.com>
2018-02-13 16:51:33 -0600

    Change

--- src/components/Album.js
+++ src/components/Album.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import albumData from './../data/albums';
+import './Album.css';
 
 class Album extends Component {
   constructor(props) {
@@ -10,8 +11,39 @@ class Album extends Component {
     });
 
     this.state = {
-      album: album
+      album: album,
+      currentSong: album.songs[0],
+      isPlaying: false
     };
+
+    this.audioElement = document.createElement('audio');
+    this.audioElement.src = album.songs[0].audioSrc;
+  }
+
+  play() {
+    this.audioElement.play();
+    this.setState({ isPlaying: true });
+  }
+
+  pause() {
+    this.audioElement.pause();
+    this.setState({ isPlaying: false });
+  }
+
+  setSong(song) {
+    this.audioElement.src = song.audioSrc;
+    this.setState({ currentSong: song });
+  }
+
+  handleSongClick(song) {
+    const isSameSong = this.state.currentSong === song;
+    if (this.state.isPlaying && isSameSong) {
+      this.pause();
+    }
+    else {
+      this.play();
+    }
+
   }
 
   render() {
@@ -32,12 +64,19 @@ class Album extends Component {
           <col id="song-duration-column" />
         </colgroup>
         <tbody className="song-name-list">
-          {this.state.album.songs.map((song, index) => {
-            console.log(song.title)
-            return <tr className="song-info" key={index}>{index + 1}.{song.title}    {song.duration}</tr>
-
-          })
-        }
+        {this.state.album.songs.map( (song, index) => 
+             <tr className="song" key={index} onClick={() => this.handleSongClick(song)} >
+               <td className="song-actions">
+                 <button>
+                   <span className="song-number">{index+1}</span>
+                   <span className="ion-play"></span>
+                   <span className="ion-pause"></span>
+                 </button>
+               </td>
+               <td className="song-title">{song.title}</td>
+               <td className="song-duration">{song.duration}</td>
+             </tr>
+           )}
         </tbody>
        </table>
       </section>
