a0f708b8d43a77887b07d4ecabc64a8eaba8de65
Yevgeniy Ivanov <anigeka@gmail.com>
2018-03-07 07:36:49 -0600

    Range Input Complete

--- src/App.js
+++ src/App.js
@@ -9,18 +9,20 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+      <header>
         <header>
         <nav>
           <Link to='/'>Landing</Link>
           <Link to='/library'>Library</Link>
         </nav>
-          <h1>Bloc Jams</h1>
+         <h1>Bloc Jams</h1>
+       </header>
+       <main>
+        <Route exact path="/" component={Landing} />
+        <Route path="/library" component={Library} />
+        <Route path="/album/:slug" component={Album} />
+       </main>
         </header>
-        <main>
-          <Route exact path="/" component={Landing} />
-          <Route path="/library" component={Library} />
-          <Route path="/album" component={Album} />
-        </main>
       </div>
     );
   }
